37419506b6541b6a9cc1b1e4b9e459b56ec709ba
Takeyuki0617 <janjagajan0@gmail.com>
2020-03-01 00:19:13 +0900

    feat: マテリアルアイコンを使い、インフォカードを作成。スタイルも設定。

--- i_am_poor/micard/micard/lib/main.dart
+++ i_am_poor/micard/micard/lib/main.dart
@@ -34,7 +34,54 @@ class MyApp extends StatelessWidget {
                 letterSpacing: 2.5,
                 fontWeight: FontWeight.bold,
               )
-            )
+            ),
+            Container(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              padding: EdgeInsets.all(10.0),
+              child: Row(children: <Widget>[
+                Icon(
+                  Icons.perm_phone_msg,
+                  color: Colors.teal
+                ),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text(
+                  '+81 1234-5678',
+                  style: TextStyle(
+                    color: Colors.teal.shade900,
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 20.0
+                  )
+                )
+              ],)
+            ),
+            Container(
+              color: Colors.white,
+              padding: EdgeInsets.all(10.0),
+              margin: EdgeInsets.symmetric(
+                vertical: 10.0,
+                horizontal: 25.0
+              ),
+              child: Row(children: <Widget>[
+                Icon(
+                  Icons.mail,
+                  color: Colors.teal,
+                ),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text(
+                  'example@email.com',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.teal.shade900,
+                    fontFamily: 'Source Sans Pro',
+                  )
+                )
+              ],)
+            ),
           ],),
         ),
       ),
