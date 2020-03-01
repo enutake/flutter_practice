6ad83b774177d567522ed749f2e5447b2e620a1e
Takeyuki0617 <janjagajan0@gmail.com>
2020-03-01 13:10:07 +0900

    feat: 変数で変動するボタンをつけた

--- dicee/dicee/lib/main.dart
+++ dicee/dicee/lib/main.dart
@@ -20,19 +20,35 @@ class MyApp extends StatelessWidget {
 }
 
 class DicePage extends StatelessWidget {
+
+  int leftDiceNumber  = 1;
+  int rightDiceNumber = 5;
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Row(
         children: <Widget>[
           Expanded(
-            child: Image(
-              image: AssetImage('images/dice1.png'),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: FlatButton(
+                onPressed: () {
+                  print('Left button got pressed.');
+                },
+                child: Image.asset('images/dice$leftDiceNumber.png')
+              ),
             ),
           ),
           Expanded(
-            child: Image(
-              image: AssetImage('images/dice1.png'),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: FlatButton(
+                onPressed: () {
+                  print('Right button got pressed.');
+                },
+                child: Image.asset('images/dice$rightDiceNumber.png')
+              ),
             ),
           ),
         ]
